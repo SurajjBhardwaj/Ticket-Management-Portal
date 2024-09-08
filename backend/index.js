668862959408321cbd90bcbc7a748350df85cda1
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2024-09-08 12:14:00 +0530

    vercel update

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const port = process.env.PORT || 5001; 
+const port = process.env.PORT || 3000; 
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
