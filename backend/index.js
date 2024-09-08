1d548b00a223ddbf6d0e4cbf6b5cea95cddfeba0
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2024-09-08 11:25:23 +0530

    inital setup backend

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 5001; 
+
+
+app.get("/", (req, res) => {
+   res.status(200).send({ data: "Hello World" });
+});
+ 
+ 
+app.listen(port, () =>{ 
+   console.log(process.env.PORT);
+  console.log(`Server running on port ${port } 🔥`)});
