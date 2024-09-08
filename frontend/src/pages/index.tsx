e1d4bc0f1819d75dae32091c841d0160ef32e886
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2024-09-08 11:25:47 +0530

    inital frontend commit

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Home() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000');
+        console.log("response", response);
+        const json = await response.json();
+        console.log(json);
+        setData(json);
+      } catch (error) {
+        setData('No data');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {data ? JSON.stringify(data) : 'No data'}
+    </div>
+  );
+}
